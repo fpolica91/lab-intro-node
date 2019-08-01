a46913e531f63ea3a47baf06bc6d08f886908ae7
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-07-31 21:45:11 -0400

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.item = []
+    this.length = 0
+
+  }
+  add(item) {
+    this.item.push(item)
+    this.item.sort((a, b) => a - b)
+    this.length = this.item.length
+
+  }
+  get(pos) {
+    return this.item[pos]
+  }
+  max() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...this.item)
+  }
+  min() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.min(...this.item)
+  }
+  average() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    let arr = this.item.reduce((a, b) => a + b, 0)
+    return arr / this.item.length;
+  }
+  sum() {
+    if (!this.item) {
+      throw new Error("EmptySortedList")
+    }
+    return this.item.reduce((a, b) => a + b, 0)
+  }
+};
+
+module.exports = SortedList;
